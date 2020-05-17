4204b398b5aa09ff618e73e8a3c28064e5a8749f
taiga0307 <tai03.ga07@gmail.com>
2020-05-17 07:40:25 +0000

    [add]CartProduct

--- app/controllers/customers/cart_products_controller.rb
+++ app/controllers/customers/cart_products_controller.rb
@@ -1,2 +1,21 @@
 class Customers::CartProductsController < ApplicationController
+
+  def show
+  	@carts = CartProduct.all # cart_product/showのカート商品一覧表示の為
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  	@cart = CartProduct.find(params[:customer_id])
+    @cart.destroy
+    redirect_to request.referer # 遷移前のURLを取得
+  end
+
+  def empty_cart
+  end
 end
