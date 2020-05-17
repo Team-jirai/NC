78fb334a5a4956d6728a6212252dbd74d884e6b9
vagrant <vagrant@localhost.localdomain>
2020-05-17 07:38:09 +0000

    [add]adminviews

--- app/controllers/admins/order_lists_controller.rb
+++ app/controllers/admins/order_lists_controller.rb
@@ -1,2 +1,17 @@
 class Admins::OrderListsController < ApplicationController
+	
+	def top
+		@order_lists = OrderList.where("created_at >= ?", Time.zone.now.beginning_of_day)
+	end
+
+	def index
+		@order_lists = OrderList.all
+	end
+
+	def show
+	end
+
+	def update
+	end
+
 end
--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,2 +1,21 @@
 class Admins::ProductsController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+	
 end
