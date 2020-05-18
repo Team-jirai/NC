ae9ecea7cb538b330f28a1a1523a04968288e7f0
vagrant <vagrant@localhost.localdomain>
2020-05-18 08:02:44 +0000

    [add]admincustomerviews

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -5,12 +5,23 @@ class Admins::CustomersController < ApplicationController
 	end
 
 	def show
+		@customer = Customer.find(params[:id])
 	end
 
 	def edit
+		@customer = Customer.find(params[:id])
 	end
 
 	def update
+		@customer = Customer.find(params[:id])
+		@customer.update(customer_params)
+		redirect_to admins_customer_path(@customer.id)
+	end
+
+	private
+
+	def customer_params
+		params.require(:customer).permit(:name_sei, :name_mei, :yomi_sei, :yomi_mei, :postal_code, :address, :tell, :email, :status)
 	end
 
 end
