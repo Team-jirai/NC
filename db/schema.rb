239f3af181c37e102e16b5914dc04b3a2b9c8adb
mohuneko <b0wdmohu@gmail.com>
2020-05-18 06:33:40 +0000

    [add]商品,index,show,top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_042759) do
+ActiveRecord::Schema.define(version: 2020_05_18_051805) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
