2842de65c736857c32dc3e3a4897b8ecddc0f1c2
vagrant <vagrant@localhost.localdomain>
2020-05-18 09:50:32 +0000

    [add]adminorderviews

--- app/controllers/admins/order_lists_controller.rb
+++ app/controllers/admins/order_lists_controller.rb
@@ -1,5 +1,5 @@
 class Admins::OrderListsController < ApplicationController
-	
+
 	def top
 		@order_lists = OrderList.where("created_at >= ?", Time.zone.now.beginning_of_day)
 	end
@@ -9,9 +9,18 @@ class Admins::OrderListsController < ApplicationController
 	end
 
 	def show
+		@order_list = OrderList.find(params[:id])
 	end
 
 	def update
+		@order_list = OrderList.find(params[:id])
+		@order_list.update(order_list_params)
+	end
+
+	private
+
+	def order_list_params
+		params.require(:order_list).permit(:status)
 	end
 
 end
