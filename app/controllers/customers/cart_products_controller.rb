73e5ddab8300a38d879575481cc77a39365b3ae5
taiga0307 <tai03.ga07@gmail.com>
2020-05-19 10:24:11 +0000

    [update]cart_products

--- app/controllers/customers/cart_products_controller.rb
+++ app/controllers/customers/cart_products_controller.rb
@@ -1,14 +1,25 @@
 class Customers::CartProductsController < ApplicationController
 
   def index
-    @cart_product = CartProduct.new(:number => 1)
+    @cart_product = CartProduct.new
   	@cart_products = CartProduct.all # cart_product/showのカート商品一覧表示の為
+    # @total_price = caluculate #privateの下に変数ある
+
+    @total_price = 0 #値を初期化している
+     current_customer.cart_products.each do |cart_product| # ログインしたユーザーに結びついているカート商品
+       @total_price = @total_price + cart_product.number * cart_product.product.price # 省略型：@total_price += cart_product.number * cart_product.product.price
+     end
+    @total_price = (@total_price * 1.1).floor # 四捨五入,@total_priceが最終型
   end
 
   def create
     @cart_product = CartProduct.new(cart_product_params)
     # CartProduct.new(cart_product_params)=受け取る箱（saveがされていない）
     @cart_product.customer_id = current_customer.id #customersの情報
+    # if @cart_product.product_id == #顧客のカートないの商品
+      # redirect_to request.referer # 遷移前のURLを取得
+    # end
+
     if @cart_product.save
     # @cart_productには１つのレコードが保存
     redirect_to customers_cart_products_path
@@ -37,10 +48,9 @@ class Customers::CartProductsController < ApplicationController
   end
 
 private
+
   def cart_product_params
-
     params.require(:cart_product).permit(:number, :product_id) #:product_idでどの商品か判断している。paramsはformから送られてきたのを許可するための記述。
-
   end
 end
 
