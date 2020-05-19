309998a417d0766ef9d09ca7e5f8e42885cc38e4
vagrant <vagrant@localhost.localdomain>
2020-05-19 09:00:52 +0000

    [fix]statusselect

--- app/models/order_list.rb
+++ app/models/order_list.rb
@@ -2,6 +2,7 @@ class OrderList < ApplicationRecord
   belongs_to :customer
   has_many :order_details
   enum status:{waitingpayment:0, paymentok:1, making:2, preparation:3, finishshipping:4}
+  # enum status:{"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4}
 
   def sum_price
   	total_price + postage
