abf075d2b20557c38ddc1906e67497f2ca819d99
mohuneko <b0wdmohu@gmail.com>
2020-05-19 01:55:59 +0000

    [編集]会員商品

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,6 @@
 class CartProduct < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+
+  number = [['1',1],['2',2],['3',3],['4',4],['5',5],['6',6],['7',7],['8',8],['9',9],['10',10]]
 end
