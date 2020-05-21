36dc32df90495179601af0154477d9497500f7ac
mohuneko <b0wdmohu@gmail.com>
2020-05-21 07:35:34 +0000

    配送先バリデーション追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,7 +3,7 @@ class ShippingAddress < ApplicationRecord
 
 
   #住所、郵便番号、宛名が空だとバリデーションかかる
-  # validates :name, :postal_code, :address, prensence: true
+  validates :name, :postal_code, :address, presence: true
 
   def full_address # orderlistで使用（住所結合表示のため）
     postal_code + " " + address + " " + name
