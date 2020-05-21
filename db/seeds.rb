5cb11108213bb8ad6254ca46ca505b19a89ce296
vagrant <vagrant@localhost.localdomain>
2020-05-21 02:03:32 +0000

    [update]admincostomerview

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+OrderList.create!(
+	postage: 800,
+	customer_id: 1,
+	total_price: 2285,
+	payment_method: 0,
+	shipping_name: "佐藤一郎",
+	postal_code: "8888888",
+	address: "アメリカワシントン州1-1-1",
+)
+
+OrderDetail.create!(
+	product_id: 1,
+	order_list_id: 1,
+	price: 495,
+	number: 3,
+)
