f8e8e8ac2592429b64cae00a91893afa7b32cf04
taiga0307 <tai03.ga07@gmail.com>
2020-05-22 07:42:12 +0000

    [update]order_list

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,6 +1,7 @@
 class CartProduct < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+  validates :number, presence: true
 
   number = [['1',1],['2',2],['3',3],['4',4],['5',5],['6',6],['7',7],['8',8],['9',9],['10',10]]
 
